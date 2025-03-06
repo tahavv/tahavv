41b7f334bb4e2d3773f7588a69a0e5363bce7c86
tahavv <spamf369@gmail.com>
2025-03-06 03:09:34 +0100

    Added GitHub Actions secrets for env vars

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,7 +21,7 @@ export default defineNuxtConfig({
     "@nuxtjs/color-mode",
   ],
   app: {
-    baseURL: "/tahavv/tahavv/",
+    baseURL: "/",
     pageTransition: { name: "page", mode: "out-in" },
     head: {
       title: "Taha Ayari | Full Stack Developer",
